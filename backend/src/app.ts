fb8320b4b93d222187d9d74d658b70797e3ec619
Hari02052000 <harisankar02052000@gmail.com>
2026-03-23 23:23:25 +0530

    husky test

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 const swaggerDocument = setupSwaggerDoc();
 app.get("/health", (_req, res) => {
-  res.status(200).json({ status: "OK" });
+  res.status(200).json({ status: "O" });
 });
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument,{
   swaggerOptions : {

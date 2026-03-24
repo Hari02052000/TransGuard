5fac9d9cb476db4cbe0afeeef42818606b322cf5
Hari02052000 <harisankar02052000@gmail.com>
2026-03-24 22:07:00 +0530

    test commit

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,4 +1,5 @@
 import globals from 'globals';
+import js from "@eslint/js";
 import tseslint from 'typescript-eslint';
 import prettier from 'eslint-config-prettier';
 import { defineConfig } from 'eslint/config';
@@ -7,10 +8,17 @@ export default defineConfig([
   {
     ignores: ['node_modules', 'dist', 'coverage'],
   },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
 
   {
     files: ['**/*.ts'],
     languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        sourceType: "module",
+        ecmaVersion: "latest",
+      },
       globals: globals.node,
     },
 
@@ -46,10 +54,6 @@ export default defineConfig([
       '@typescript-eslint/explicit-function-return-type': 'off', 
     },
 
-    extends: ['js/recommended'],
   },
-
-  ...tseslint.configs.recommended,
-
   prettier,
 ]);
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,20 +1,23 @@
-import express from "express";
-import swaggerUi from "swagger-ui-express";
-import { setupSwaggerDoc } from "./config/swager";
-import { v1Router }  from '@src/config/routes';
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import { setupSwaggerDoc } from './config/swager';
+import { v1Router } from '@src/config/routes';
 
 const app = express();
 app.use(express.json());
 const swaggerDocument = setupSwaggerDoc();
-app.get("/health", (_req, res) => {
-  res.status(200).json({ status: "Ok" });
+app.get('/health', (_req, res) => {
+  res.status(200).json({ status: 'O' });
 });
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument,{
-  swaggerOptions : {
-    persistAuthorization: true
-  }
-}));
-app.use("/api/v1", v1Router);
-
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  }),
+);
+app.use('/api/v1', v1Router);
 
 export default app;

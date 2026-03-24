dd8a43e1e26828bff524e3b71b6774da600db47c
Hari02052000 <harisankar02052000@gmail.com>
2026-03-24 22:19:46 +0530

    test commit

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 const swaggerDocument = setupSwaggerDoc();
 app.get('/health', (_req, res) => {
-  res.status(200).json({ status: 'O' });
+  res.status(200).json({ status: 'OK' });
 });
 app.use(
   '/api-docs',
